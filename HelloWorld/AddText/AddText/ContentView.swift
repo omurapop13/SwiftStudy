7a66f9d5fcc75a05be75c6581f589258fbe046d8
Kazuyuki Ohmura <omurapop13@gmail.com>
2020-10-08 00:55:48 +0900

    tst

---
+++ HelloWorld/AddText/AddText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddText
+//
+//  Created by Bubusama on 2020/05/28.
+//  Copyright © 2020 Bubusama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Bubusama on 2020/05/16.
+//  Copyright © 2020 Bubusama. All rights reserved.
+//
+
+/*
+ Another commnets
+ */
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
